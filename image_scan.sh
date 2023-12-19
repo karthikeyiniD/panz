534c233be2e22355a9f20bb042c94a1e072ae56c
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-19 19:15:05 +0530

    Update image_scan.sh

--- image_scan.sh
+++ image_scan.sh
@@ -2,8 +2,8 @@
 repo_name=demo
 image_tag=demo-project-${BUILD_NUMBER}
 arn="arn:aws:sns:us-east-1:771070158678:demo"
-critical_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag --region us-east-2 | grep -i "findingSeverityCounts" -A 5 | grep -i critical | cut -d ":" -f 2 | tr -d ",")
-high_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag --region us-east-2 | grep -i "findingSeverityCounts" -A 5 | grep -i high | cut -d ":" -f 2 | tr -d ",")
+critical_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag --region us-east-1 | grep -i "findingSeverityCounts" -A 5 | grep -i critical | cut -d ":" -f 2 | tr -d ",")
+high_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag --region us-east-1 | grep -i "findingSeverityCounts" -A 5 | grep -i high | cut -d ":" -f 2 | tr -d ",")
 
 if [ -z "$critical_vulnr" ]; then
     critical_vulnr=0
@@ -18,7 +18,7 @@ echo "Critical vulnerabilities: $critical_vulnr"
 
 if [[ $high_vulnr -gt 0 || $critical_vulnr -gt 0 ]]; then
    echo "Your image is having vulnerabilities, please check...."
-   aws sns publish --topic-arn $arn --region us-east-2 --message "Your image in this $repo_name repo with image tag $image_tag has vulnerabilities, please check..."
+   aws sns publish --topic-arn $arn --region us-east-1 --message "Your image in this $repo_name repo with image tag $image_tag has vulnerabilities, please check..."
    exit 1
 else
     echo "Your image is safe for deployment"

7c864593743b2525813482388469841ce9d88386
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-21 22:25:41 +0530

    Create dependency_check.sh

---
+++ dependency_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+Dependency_Track_Project_Identity="identity"
+Dependency_Track_Project_Token="odt_abnEZmAyN07mZjLfLXCLUxpxcNaBl58w"
+syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG-Dev-$BUILD_NUMBER -o cyclonedx > syft_scanresults
+apk add --no-cache jq
+BOM_CONTENT_BASE64=$(base64 -w0 syft_scanresults)
+echo '{"project": "'"$Dependency_Track_Project_Identity"'", "bom": "'"$BOM_CONTENT_BASE64"'"}' > json_payload.json
+JSON_PAYLOAD=$(jq -n --slurpfile json json_payload.json '$json[0]')
+curl -X "PUT" "https://www.karthikeyini.tech/api/v1/bom" \
+   -H "Content-Type: application/json" \
+   -H "X-API-Key: ${Dependency_Track_Project_Token}" \
+   --data @json_payload.json

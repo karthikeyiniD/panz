0fc6d3de29f03f28193722253a52cd9267a6813d
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-21 22:58:50 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 Dependency_Track_Project_Identity="identity"
 Dependency_Track_Project_Token="odt_abnEZmAyN07mZjLfLXCLUxpxcNaBl58w"
-DOCKER_REGISTRY=771070158678.dkr.ecr.us-east-2.amazonaws.com/demo
-REPO_DEV_NAME=deno
-ECR_TAG=demo
+DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
+REPO_DEV_NAME="demo"
+ECR_TAG="demo-project"
 syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG-Dev-$BUILD_NUMBER -o cyclonedx > syft_scanresults
 apk add --no-cache jq
 BOM_CONTENT_BASE64=$(base64 -w0 syft_scanresults)

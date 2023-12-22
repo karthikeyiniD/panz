d2af166b790defb90853dce248d8bbabf733347b
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-22 13:27:53 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-Dependency_Track_Project_Identity="identity"
-Dependency_Track_Project_Token="token"
-syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG-Dev-$BUILD_NUMBER -o cyclonedx > syft_scanresults
+Dependency_Track_Project_Identity="34b86dad-e34c-4c9a-878b-f3a9ebe8abd7"
+Dependency_Track_Project_Token="odt_ysKTEzSEI1mEjTunwfugps3LgYeEbjME"
+DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
+REPO_DEV_NAME="demo"
+ECR_TAG="demo-project-12"
+syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > syft_scanresults
 apk add --no-cache jq
 BOM_CONTENT_BASE64=$(base64 -w0 syft_scanresults)
 echo '{"project": "'"$Dependency_Track_Project_Identity"'", "bom": "'"$BOM_CONTENT_BASE64"'"}' > json_payload.json

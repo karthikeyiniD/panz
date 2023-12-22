86e4a1203156ae1a3bcb0f29d2ea47f686547040
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-22 13:26:27 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 Dependency_Track_Project_Identity="identity"
-Dependency_Track_Project_Token="odt_abnEZmAyN07mZjLfLXCLUxpxcNaBl58w"
-DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
-REPO_DEV_NAME="demo"
-ECR_TAG="demo-project"
+Dependency_Track_Project_Token="token"
 syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG-Dev-$BUILD_NUMBER -o cyclonedx > syft_scanresults
 apk add --no-cache jq
 BOM_CONTENT_BASE64=$(base64 -w0 syft_scanresults)

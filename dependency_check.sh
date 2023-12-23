8c4cdbf29311113dd75a46d5d9f655d180fec74d
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-23 20:16:09 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -3,8 +3,9 @@ Dependency_Track_Project_Identity="608fb4c8-3202-4d5c-94d1-610bd6f3354a"
 Dependency_Track_Project_Token="odt_uh8kitt8MyRMYvc7utwG3I7dFOElw2Kg"
 DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
 REPO_DEV_NAME="demo"
-ECR_TAG="demo-project-12"
-syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > syft_scanresults
+ECR_TAG="demo-project"
+syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG-Dev-$BUILD_NUMBER -o cyclonedx > syft_scanresults
+apk add --no-cache jq
 BOM_CONTENT_BASE64=$(base64 -w0 syft_scanresults)
 echo '{"project": "'"$Dependency_Track_Project_Identity"'", "bom": "'"$BOM_CONTENT_BASE64"'"}' > json_payload.json
 JSON_PAYLOAD=$(jq -n --slurpfile json json_payload.json '$json[0]')

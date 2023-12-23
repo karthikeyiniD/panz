50a559ccea77209df5a8ae2576fd138d457106df
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-23 21:47:04 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Dependency_Track_Project_Identity="608fb4c8-3202-4d5c-94d1-610bd6f3354a"
-# Dependency_Track_Project_Token="odt_1zWhBi84Bl0g2Zj7SukIN2cjq4UuFPUD"
+Dependency_Track_Project_Identity="608fb4c8-3202-4d5c-94d1-610bd6f3354a"
+Dependency_Track_Project_Token="odt_1zWhBi84Bl0g2Zj7SukIN2cjq4UuFPUD"
 DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
 REPO_DEV_NAME="demo"
-ECR_TAG="demo-project-8"
+ECR_TAG="demo-project-9"
 syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > syft_scanresults
 BOM_CONTENT_BASE64=$(base64 -w0 syft_scanresults)
 echo '{"project": "'"$Dependency_Track_Project_Identity"'", "bom": "'"$BOM_CONTENT_BASE64"'"}' > json_payload.json

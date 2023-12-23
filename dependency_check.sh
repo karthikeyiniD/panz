980ede515706a55443c3fb7cc5fe084b95c5304a
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-23 20:11:50 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-Dependency_Track_Project_Identity="34b86dad-e34c-4c9a-878b-f3a9ebe8abd7"
-Dependency_Track_Project_Token="odt_ysKTEzSEI1mEjTunwfugps3LgYeEbjME"
+Dependency_Track_Project_Identity="608fb4c8-3202-4d5c-94d1-610bd6f3354a"
+Dependency_Track_Project_Token="odt_uh8kitt8MyRMYvc7utwG3I7dFOElw2Kg"
 DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
 REPO_DEV_NAME="demo"
 ECR_TAG="demo-project-12"

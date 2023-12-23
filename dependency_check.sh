2f643d920323fd778cdc0f54fa2fbbe9b99ae9a0
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-23 22:36:58 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -8,7 +8,7 @@ syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > syf
 BOM_CONTENT_BASE64=$(base64 -w0 syft_scanresults)
 echo '{"project": "'"$Dependency_Track_Project_Identity"'", "bom": "'"$BOM_CONTENT_BASE64"'"}' > json_payload.json
 JSON_PAYLOAD=$(jq -n --slurpfile json json_payload.json '$json[0]')
-curl -X "POST" "https://www.karthikeyini.tech/v1/bom" \
+curl -X "PUT" "https://www.karthikeyini.tech/api/v1/bom" \
    -H "Content-Type: application/json" \
    -H "X-API-Key: ${Dependency_Track_Project_Token}" \
    --data @json_payload.json

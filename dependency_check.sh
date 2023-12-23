7744403b8e01ea97f56a549956903ba3090fc3ba
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-24 00:25:53 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -5,7 +5,7 @@ Dependency_Track_Project_Identity="608fb4c8-3202-4d5c-94d1-610bd6f3354a"
 Dependency_Track_Project_Token="odt_9sbFtp7Ws0mzyadqZw8zL2FEOc5ODPJH"
 DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
 REPO_DEV_NAME="demo"
-ECR_TAG="demo-project-20"
+ECR_TAG="demo-project-24"
 
 # Run Syft to generate SBOM
 syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > syft_scanresults
@@ -20,7 +20,7 @@ echo '{"project": "'"$Dependency_Track_Project_Identity"'", "bom": "'"$BOM_CONTE
 JSON_PAYLOAD=$(jq -n --slurpfile json json_payload.json '$json[0]')
 
 # Send SBOM to Dependency-Track
-curl -X "GET" "https://www.karthikeyini.tech/api/v1/bom" \
+curl -X "PUT" "https://www.karthikeyini.tech/api/v1/bom" \
    -H "Content-Type: application/json" \
    -H "X-API-Key: ${Dependency_Track_Project_Token}" \
    --data @json_payload.json

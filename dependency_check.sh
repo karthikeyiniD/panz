60d88def4ee791ba3ca1d8f40ba033e9781736a1
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-25 00:41:48 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -1,31 +1,3 @@
-# #!/bin/bash
-
-# # Set your variables
-# Dependency_Track_Project_Identity="1f00af01-2df8-4b68-a0ee-6781adfe0c83"
-# Dependency_Track_Project_Token="odt_RtE9ROsIe5QlJZvi18xoLE0AyzKIVQjf"
-# DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
-# REPO_DEV_NAME="demo"
-# ECR_TAG="demo-project-81"
-
-# # Run Syft to generate SBOM
-# syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > syft_scanresults
-
-# # Convert SBOM content to base64
-# BOM_CONTENT_BASE64=$(base64 -w0 syft_scanresults)
-
-# # Create JSON payload for Dependency-Track API
-# echo '{"project": "'"$Dependency_Track_Project_Identity"'", "bom": "'"$BOM_CONTENT_BASE64"'"}' > json_payload.json
-
-# # Prepare JSON payload using jq
-# JSON_PAYLOAD=$(jq -n --slurpfile json json_payload.json '$json[0]')
-
-# # Send SBOM to Dependency-Track
-# curl -X "PUT" "https://api.karthikeyini.tech" \
-#    -H "Content-Type: application/json" \
-#    -H "X-API-Key: ${Dependency_Track_Project_Token}" \
-#    --data @json_payload.json
-
-
 #!/bin/bash
 
 # Set your variables
@@ -33,26 +5,23 @@ Dependency_Track_Project_Identity="1f00af01-2df8-4b68-a0ee-6781adfe0c83"
 Dependency_Track_Project_Token="odt_RtE9ROsIe5QlJZvi18xoLE0AyzKIVQjf"
 DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
 REPO_DEV_NAME="demo"
-ECR_TAG="demo-project-82"
+ECR_TAG="demo-project-81"
 
-# Run Syft to generate SBOM and log errors
-syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > syft_scanresults 2> syft_error.log
+# Run Syft to generate SBOM
+syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > syft_scanresults
 
-# Debugging: Log BOM_CONTENT_BASE64
+# Convert SBOM content to base64
 BOM_CONTENT_BASE64=$(base64 -w0 syft_scanresults)
-echo "BOM_CONTENT_BASE64: $BOM_CONTENT_BASE64" > debug.log
 
 # Create JSON payload for Dependency-Track API
 echo '{"project": "'"$Dependency_Track_Project_Identity"'", "bom": "'"$BOM_CONTENT_BASE64"'"}' > json_payload.json
 
-# Debugging: Log JSON_PAYLOAD
+# Prepare JSON payload using jq
 JSON_PAYLOAD=$(jq -n --slurpfile json json_payload.json '$json[0]')
-echo "JSON_PAYLOAD: $JSON_PAYLOAD" >> debug.log
 
 # Send SBOM to Dependency-Track
-curl -X "PUT" "https://api.karthikeyini.tech/projects/1f00af01-2df8-4b68-a0ee-6781adfe0c83" \
+curl -X "PUT" "https://api.karthikeyini.tech" \
    -H "Content-Type: application/json" \
    -H "X-API-Key: ${Dependency_Track_Project_Token}" \
    --data @json_payload.json
-
 

48e2c566439c554c7c8addc10116b27814b956a1
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-25 00:43:35 +0530

    Create syft.sh

---
+++ syft.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
+REPO_DEV_NAME="demo"
+ECR_TAG="demo-project-changebuildnumber"
+
+# Run Syft to generate SBOM
+syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > bom.json

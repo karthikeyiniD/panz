71802e129158905b84e31d01bd33cd7dadefe2c0
karthikeyiniD <128837288+karthikeyiniD@users.noreply.github.com>
2023-12-24 23:11:07 +0530

    Update dependency_check.sh

--- dependency_check.sh
+++ dependency_check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set your variables
-Dependency_Track_Project_Identity="70009411-9135-4bd4-8618-40d8cf252157"
+Dependency_Track_Project_Identity="1f00af01-2df8-4b68-a0ee-6781adfe0c83"
 Dependency_Track_Project_Token="odt_RtE9ROsIe5QlJZvi18xoLE0AyzKIVQjf"
 DOCKER_REGISTRY="771070158678.dkr.ecr.us-east-2.amazonaws.com"
 REPO_DEV_NAME="demo"
-ECR_TAG="demo-project-78"
+ECR_TAG="demo-project-81"
 
 # Run Syft to generate SBOM
 syft packages docker:$DOCKER_REGISTRY/$REPO_DEV_NAME:$ECR_TAG -o cyclonedx > syft_scanresults
@@ -20,7 +20,7 @@ echo '{"project": "'"$Dependency_Track_Project_Identity"'", "bom": "'"$BOM_CONTE
 JSON_PAYLOAD=$(jq -n --slurpfile json json_payload.json '$json[0]')
 
 # Send SBOM to Dependency-Track
-curl -X "PUT" "https://api.karthikeyini.tech/" \
+curl -X "PUT" "https://api.karthikeyini.tech" \
    -H "Content-Type: application/json" \
    -H "X-API-Key: ${Dependency_Track_Project_Token}" \
    --data @json_payload.json
